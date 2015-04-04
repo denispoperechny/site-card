77323d89d7ece8774b9b2357ced6f647dde8215b
denis <denispoperechny@gmail.com>
2015-04-04 06:34:16 -0700

    Projects section added

--- www/script/log.js
+++ www/script/log.js
@@ -2,9 +2,5 @@ Log("log.php");
 
 function Log(logUrl)
 {
-    var xmlHttp = null;
-
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", logUrl, false );
-    xmlHttp.send( null );
+    $.get( logUrl, function( data ) { });
 }
